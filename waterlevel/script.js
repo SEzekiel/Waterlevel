bca98b74e61fef5cf0111b965c9f5659f5251e44
JamesMugo <james.mugo@ashesi.edu.gh>
2017-03-14 01:41:33 +0000

    added a javascript function

--- waterlevel/script.js
+++ waterlevel/script.js
@@ -1,5 +1,13 @@
-function pumpWater(){
 
+
+
+function pumpWater(){
+	var pump = ["level1","level2","level3","level4","level5","level6","level7","level8","level9","level10"];
+	
+	for (var i = 0; i < pump.length; i++) {
+		pump[i].fillStyle = "blue";
+		document.getElementById("pumping").innerHTML = pump[i].fill();
+	}
 }
 
 function drawWater(){
