b3804e82173cd17dfc25d7e1d8dc0d150c0ab4ce
alieu <alieujallow93@gmail.com>
2017-03-14 01:55:46 +0000

    hello

--- waterlevel/index.php
+++ waterlevel/index.php
@@ -4,17 +4,38 @@
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<title>Home</title>
-	<link rel="stylesheet" href="">
+	<link rel="stylesheet" href="style.css">
 	<!-- Latest compiled and minified CSS -->
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
 
 
 </head>
 <body>
+	<div class="container-fluid">
+		<div class="row" id="row1">
+			<div class="col-xs-4" id="tankWrapper">
+				<div id="tank" class="row">
+					<div id="level1"></div>
+					<div id="level2"></div>
+					<div id="level3"></div>
+					<div id="level4"></div>
+					<div id="level5"></div>
+					<div id="level6"></div>
+					<div id="level7"></div>
+					<div id="level8"></div>
+					<div id="level9"></div>
+					<div id="level10"></div>
+				</div>
+				<div class="row" id="buttonSpace">
+					<!-- Indicates a dangerous or potentially negative action -->
+					<button  class="btn btn-success btn-lg " id="fetchbtn"  type="button">Fetch</button>
+					<button type="button" class="btn btn-danger btn-lg" id="stopbtn">Stop</button>
+					
+				</div>
+			</div>
+		</div>
+	</div>
 	
-
-
-
 	<!-- jQuery library -->
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
 
